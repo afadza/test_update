e7ab13cfbf199aac3268d4a19c1396854e484e0d
Arya Fadhilah Dzaki <arya.digitels@gmail.com>
2025-02-04 11:49:24 +0800

    update

--- App.tsx
+++ App.tsx
@@ -1,12 +1,60 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {StyleSheet, View, Text, Button, Image} from 'react-native';
+import {useCheckVersion} from './useCheckVersion';
 
-const App = () => {
+export default function App() {
+  const {version} = useCheckVersion();
+
   return (
-    <View>
-      <Text>HELLO TEST UPDATE</Text>
+    <View style={styles.container}>
+      <Text>Result: 555555</Text>
+      <Button title={'check update Git'} onPress={version.onCheckGitVersion} />
+
+      {version.state.loading && <Text>Loading from git...</Text>}
+      {!!version.state.progress && (
+        <View style={styles.progress}>
+          <View
+            style={[
+              styles.process,
+              {
+                width: `${version.state.progress}%`,
+              },
+            ]}
+          />
+        </View>
+      )}
     </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+  progress: {
+    height: 10,
+    width: '80%',
+    marginTop: 20,
+    borderRadius: 8,
+    borderColor: 'grey',
+    borderWidth: 1,
+    overflow: 'hidden',
+  },
+  process: {
+    height: 10,
+    backgroundColor: 'blue',
+  },
+  img: {
+    width: 180,
+    height: 180,
+    resizeMode: 'center',
+    marginBottom: 20,
+  },
+});
---
+++ useCheckVersion.ts
@@ -0,0 +1,91 @@
+import hotUpdate from 'react-native-ota-hot-update';
+import {Alert, LayoutAnimation, Platform, UIManager} from 'react-native';
+import ReactNativeBlobUtil from 'react-native-blob-util';
+import React from 'react';
+if (Platform.OS === 'android') {
+  if (UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+}
+export const useCheckVersion = () => {
+  const [progress, setProgress] = React.useState(0);
+  const [loading, setLoading] = React.useState(false);
+
+  const onCheckGitVersion = () => {
+    setProgress(0);
+    setLoading(true);
+    hotUpdate.git.checkForGitUpdate({
+      branch: Platform.OS === 'ios' ? 'iOS' : 'android',
+      bundlePath:
+        Platform.OS === 'ios'
+          ? 'output/main.jsbundle'
+          : 'output/index.android.bundle',
+      url: 'https://github.com/afadza/ota_update.git',
+      onCloneFailed(msg: string) {
+        Alert.alert('Clone project failed!', msg, [
+          {
+            text: 'Cancel',
+            onPress: () => {},
+            style: 'cancel',
+          },
+        ]);
+      },
+      onCloneSuccess() {
+        Alert.alert('Clone project success!', 'Restart to apply the changing', [
+          {
+            text: 'ok',
+            onPress: () => hotUpdate.resetApp(),
+          },
+          {
+            text: 'Cancel',
+            onPress: () => {},
+            style: 'cancel',
+          },
+        ]);
+      },
+      onPullFailed(msg: string) {
+        Alert.alert('Pull project failed!', msg, [
+          {
+            text: 'Cancel',
+            onPress: () => {},
+            style: 'cancel',
+          },
+        ]);
+      },
+      onPullSuccess() {
+        Alert.alert('Pull project success!', 'Restart to apply the changing', [
+          {
+            text: 'ok',
+            onPress: () => hotUpdate.resetApp(),
+          },
+          {
+            text: 'Cancel',
+            onPress: () => {},
+            style: 'cancel',
+          },
+        ]);
+      },
+      onProgress(received: number, total: number) {
+        const percent = (+received / +total) * 100;
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        setProgress(percent);
+      },
+      onFinishProgress() {
+        setLoading(false);
+      },
+    });
+  };
+  const removeGitUpdate = () => {
+    hotUpdate.git.removeGitUpdate();
+  };
+  return {
+    version: {
+      onCheckGitVersion,
+      removeGitUpdate,
+      state: {
+        progress,
+        loading,
+      },
+    },
+  };
+};
