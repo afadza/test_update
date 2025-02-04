1f1d0d544af9417693230349122cf7e586701b84
Arya Fadhilah Dzaki <arya.digitels@gmail.com>
2025-02-04 15:40:12 +0800

    change

--- useCheckVersion.ts
+++ useCheckVersion.ts
@@ -15,12 +15,12 @@ export const useCheckVersion = () => {
     setProgress(0);
     setLoading(true);
     hotUpdate.git.checkForGitUpdate({
-      branch: Platform.OS === 'ios' ? 'iOS' : 'android',
+      branch: 'main',
       bundlePath:
         Platform.OS === 'ios'
           ? 'output/main.jsbundle'
-          : 'output/index.android.bundle',
-      url: 'https://github.com/afadza/ota_update.git',
+          : 'android/output/index.android.bundle',
+      url: 'https://github.com/afadza/test_update',
       onCloneFailed(msg: string) {
         Alert.alert('Clone project failed!', msg, [
           {
