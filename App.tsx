f3efde836076fc20bffcfde0df49dd2897338c07
Arya Fadhilah Dzaki <arya.digitels@gmail.com>
2025-02-07 09:26:26 +0800

    update

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <img src="./src/img/sinchan.png" style={styles.img} />
+      <Image source={require('./src/assets/sinchan.png')} style={styles.img} />
       <Text>AH MASA KE UPDATE</Text>
       <Button title={'check update Git'} onPress={version.onCheckGitVersion} />
       {version.state.loading && <Text>Loading from git...</Text>}
